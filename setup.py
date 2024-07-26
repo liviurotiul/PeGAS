ba01ef2cc5ddb1625a7b5d0981f00502d86b9a68
Liviu Rotaru <liviurotiultelefon@gmail.com>
2024-07-26 13:20:37 +0300

    changed version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class CustomInstallCommand(install):
 
 setup(
     name='pegas',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
