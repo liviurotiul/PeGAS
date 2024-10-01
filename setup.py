351596a348e81d02b1904708fe07250e4483ee8d
Liviu Rotaru <liviurotiultelefon@gmail.com>
2024-10-01 11:22:22 +0300

    modified version and utils

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class CustomInstallCommand(install):
 
 setup(
     name='pegas',
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
