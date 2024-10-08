93d41e8d12ee320953626f59aaba96627268e697
Liviu Rotaru <liviurotiultelefon@gmail.com>
2024-10-08 11:07:15 +0300

    dsadasda

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class CustomInstallCommand(install):
 
 setup(
     name='pegas',
-    version='0.2.7',
+    version='0.2.9',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
