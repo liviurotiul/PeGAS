74ba586ccb0822266d2c6a22582eec9fb953decb
Liviu Rotaru <liviurotiultelefon@gmail.com>
2026-01-19 10:39:06 +0200

    Chaged version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class CustomInstallCommand(install):
 
 setup(
     name='pegas',
-    version='1.2.0',
+    version='1.2.1',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
