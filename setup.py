b4adf75c77b05407fbc6bbd906d93427c73c9ff7
Liviu Rotaru <liviurotiultelefon@gmail.com>
2026-02-27 10:43:37 +0200

    Chnaged version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class CustomInstallCommand(install):
 
 setup(
     name='pegas',
-    version='1.2.3',
+    version='1.2.4',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
